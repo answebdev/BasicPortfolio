e8c92939318d444964e12a83d65ce3ceb0c8e99c
ANS <36783010+answebdev@users.noreply.github.com>
2022-02-28 15:31:49 -0800

    add tests

--- cypress/integration/tests/first-tests.test.js
+++ cypress/integration/tests/first-tests.test.js
@@ -1,13 +1,19 @@
 /// <reference types="cypress" />
 
 describe('First Test Suite', () => {
-  it('test one', () => {
+  it('Quick Site Test', () => {
     cy.visit('https://answebdev.github.io/BasicPortfolio/');
+    cy.log('Checking that there is an About Me header');
     cy.contains('About Me').should('exist');
+
     cy.contains('Portfolio').click();
-    cy.log('Checking URL for /portfolio page');
+    cy.log('Checking URL for Portfolio page');
     cy.url().should('include', '/portfolio');
+
     cy.contains('Contact').click();
+    cy.log('Checking URL for Contact page');
+    cy.url().should('include', '/contact');
+    cy.log('Checking contact form');
     cy.get('[data-test=name]').type('ABRACADABRA');
     cy.get('[data-test=email]').type('abracadabra@gmail.com');
     cy.get('[data-test=message]').type(
